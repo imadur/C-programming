044f7c672f408ce1bbbb713e39d7b8fd80aba31d
imadur <156501712+imadur@users.noreply.github.com>
2024-01-13 20:39:24 +0600

    sum of two numbers

---
+++ sumTWO.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b,sum;
+    printf("Enter 1st Number: ");
+    scanf("%d",&a);
+    printf("Enter The 2nd Number: ");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("sum of two numbers is:%d",sum);
+    return 0;
+}
