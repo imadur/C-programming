e5db2528dcf679b8483e417cbfc7664d4dc91629
imadur <156501712+imadur@users.noreply.github.com>
2024-01-13 14:32:43 +0600

    char input test

--- testC.c
+++ testC.c
@@ -3,9 +3,16 @@
 int main() {
     printf("Hello world\n");
 
+    //number input test
     int n;
     printf("Enter a number: ");
     scanf("%d", &n);
-    printf("your number is : %d \n", n);
+    printf("your number is : %d\n", n);
+
+    // char input test
+    char c;
+    printf("enter a char: ");
+    scanf(" %c", &c);
+    printf("your char is %c \n", c);
 }
 
